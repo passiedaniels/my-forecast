f1b7aefb3d00b2a59bde36b6a081725533eac4f6
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-05-24 01:11:24 +0300

    added changes to css

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchBox = document.querySelector("#searchbox");
-  cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchBox.value;
-}
-
-let formTypeElement = document.querySelector("#form-type");
-formTypeElement.addEventListener("submit", handleSearchSubmit);
